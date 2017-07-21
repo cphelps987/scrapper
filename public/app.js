79a3e698e486955369c643fae595bcb08fe550f4
cphelps987 <cphelps987@gmail.com>
2017-07-21 14:26:43 -0400

    got it to delete

--- public/app.js
+++ public/app.js
@@ -80,33 +80,7 @@ $(document).on("click", "#savenote", function() {
 });
 
 $(document).on("click", "#deletenote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
 
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-            // Value taken from title input
-            title   : $("#titleinput").remove(),
-            // Value taken from note textarea
-            body    : $("#bodyinput").remove(),
-            button  : $("#deletenote").remove(),
-            button  : $("#savenote").remove(),
-            h2      : $("#h2").remove(),
-        }
-    })
-    // With that done
-        .done(function(data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-            $("#notes").remove();
-        });
-
-    // Also, remove the values entered in the input and textarea for note entry
     $("#titleinput").val("");
     $("#bodyinput").val("");
-    $("#notes").empty();
 });
